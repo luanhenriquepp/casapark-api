4f536b75c33e782aa747728789a25b6f9b4e8226
Luan Henrique <luan.henriqueepp@gmail.com>
2020-06-24 23:57:47 -0300

    bastera de teste

--- config/filesystems.php
+++ config/filesystems.php
@@ -46,12 +46,12 @@ return [
         'local' => [
             'driver' => 'local',
             'url' => env('APP_URL').'/storage',
-            'root' => storage_path('app'),
+            'root' => storage_path('app/public'),
         ],
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path(''),
+            'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
