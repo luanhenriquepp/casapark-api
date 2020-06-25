ec871bba9add128e7167680d03d600031d8a120d
Luan Henrique <luan.henriqueepp@gmail.com>
2020-06-24 23:39:25 -0300

    outro teste

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,7 +51,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root' => storage_path(''),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
